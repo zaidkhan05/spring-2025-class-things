ba5cac2af09b1a1d270efc3790e7fbdec2e5049b
Zaid Khan <115332749+zaidkhan05@users.noreply.github.com>
2025-02-25 20:14:14 -0500

    rserfsx

--- CS4306/assn3.py
+++ CS4306/assn3.py
@@ -1,4 +1,4 @@
-import networkx as nx
+import time
 
 def dfs_topological_sort(graph):
     visited = set()  # To track visited nodes
@@ -53,38 +53,55 @@ def topological_sort_kahn(graph):
 
 # Define the graph using adjacency list
 graph1 = {
-    'a': ['b', 'c'],
-    'b': ['e', 'g'],
-    'c': ['f'],
-    'd': ['a', 'b', 'f', 'g'],
-    'e': [],
-    'f': [],
-    'g': ['f', 'e']
+    'a': ['b'],
+    'b': ['g'],
+    'c': ['d', 'e'],
+    'd': ['b', 'g'],
+    'e': ['h'],
+    'f': ['a', 'b'],
+    'g': ['f'],
+    'h': ['c', 'd']
 }
 
+
 graph2 = {
-    'a': ['b'],
-    'b': ['c'],
-    'c': ['d'],
-    'd': ['g'],
-    'e': ['a'],
-    'f': ['e', 'b', 'c', 'g'],
-    'g': ['e']
+    'a': ['f'],
+    'b': ['a', 'd', 'f'],
+    'c': ['h'],
+    'd': ['c', 'h'],
+    'e': ['c'],
+    'f': ['g'],
+    'g': ['b', 'd'],
+    'h': ['e']
 }
+
+
 
 
 
 # Apply topological sorting
+sortTime = time.perf_counter()
 topological_ordera = dfs_topological_sort(graph1)
+sortTime = time.perf_counter() - sortTime
+print(f"DFS Time for graph a:{sortTime:.8f} seconds")
+sortTime = time.perf_counter()
 topological_orderb = dfs_topological_sort(graph2)
+sortTime = time.perf_counter() - sortTime
+print(f"DFS Time for graph a:{sortTime:.8f} seconds")
 
+sortTime = time.perf_counter()
 kahn_ordera = topological_sort_kahn(graph1)
+sortTime = time.perf_counter() - sortTime
+print(f"Kahns Time for graph a:{sortTime:.8f} seconds")
+sortTime = time.perf_counter()
 kahn_orderb = topological_sort_kahn(graph2)
+sortTime = time.perf_counter() - sortTime
+print(f"Kahns Time for graph a:{sortTime:.8f} seconds")
 
-print("Topological Order for graph a:", topological_ordera)
+print("dfs Order for graph a:", topological_ordera)
 
-print("Topological Order for graph b:", topological_orderb)
+print("dfs Order for graph b:", topological_orderb)
 
-print("Kahn's Algorithm Order for graph a:", kahn_ordera)
+print("Kahns Algorithm Order for graph a:", kahn_ordera)
 
-print("Kahn's Algorithm Order for graph b:", kahn_orderb)
+print("Kahns Algorithm Order for graph b:", kahn_orderb)
